670c17bfd7d1df10a6776343d12eeaa2590c599e
eaarda <elifaarda@gmail.com>
2020-12-22 01:16:22 +0300

    homework-1 done

---
+++ homework1.py
@@ -0,0 +1,23 @@
+
+def getValues():
+    values = []
+    for i in range(0, 5):
+        # input methods always takes string types
+        value = input('Please enter a value: ')
+        values.append(value)
+    return values
+
+
+def printValues(list):
+    for i in range(0, 5):
+        print(f'{i+1}st value: {list[i]} and type: {type(list[i])} ')
+
+
+def main():
+    print("Hi, user!")
+    list = getValues()
+    printValues(list)
+
+
+if __name__ == "__main__":
+    main()
